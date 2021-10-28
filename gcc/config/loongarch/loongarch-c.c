8b7e7945dcb95849ec69046a05145c55e4ecff1a
chenglulu <chenglulu@loongson.cn>
2021-10-28 10:41:24 +0800

    LoongArch Port: gcc
    gcc/
    
    	* common/config/loongarch/loongarch-common.c: New file.
    	* config/loongarch/constraints.md: New file.
    	* config/loongarch/generic.md: New file.
    	* config/loongarch/gnu-user.h: New file.
    	* config/loongarch/la464.md: New file.
    	* config/loongarch/larchintrin.h: New file.
    	* config/loongarch/linux.h: New file.
    	* config/loongarch/loongarch-builtins.c: New file.
    	* config/loongarch/loongarch-c.c: New file.
    	* config/loongarch/loongarch-cpu.c: New file.
    	* config/loongarch/loongarch-cpu.h: New file.
    	* config/loongarch/loongarch-driver.c: New file.
    	* config/loongarch/loongarch-driver.h: New file.
    	* config/loongarch/loongarch-ftypes.def: New file.
    	* config/loongarch/loongarch-modes.def: New file.
    	* config/loongarch/loongarch-opts.c: New file.
    	* config/loongarch/loongarch-opts.h: New file.
    	* config/loongarch/loongarch-protos.h: New file.
    	* config/loongarch/loongarch-rtx-cost.h: New file.
    	* config/loongarch/loongarch.c: New file.
    	* config/loongarch/loongarch.h: New file.
    	* config/loongarch/loongarch.md: New file.
    	* config/loongarch/loongarch.opt: New file.
    	* config/loongarch/predicates.md: New file.
    	* config/loongarch/sync.md: New file.
    	* config/loongarch/t-linux: New file.
    	* config/loongarch/t-loongarch: New file.
    	* config.gcc: Add LoongArch support.
    	* config/host-linux.c: Add LoongArch support.
    	* configure.ac: Add LoongArch support.
    	* doc/invoke.texi: Add LoongArch options section.
    	* doc/md.texi: Add LoongArch constraints section.

---
+++ gcc/config/loongarch/loongarch-c.c
@@ -0,0 +1,137 @@
+/* LoongArch-specific code for C family languages.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   Contributed by Loongson Ltd.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#define IN_TARGET_CODE 1
+
+#include "config.h"
+#include "system.h"
+#include "coretypes.h"
+#include "tm.h"
+#include "c-family/c-common.h"
+#include "cpplib.h"
+
+#define preprocessing_asm_p() (cpp_get_options (pfile)->lang == CLK_ASM)
+#define builtin_define(TXT) cpp_define (pfile, TXT)
+#define builtin_assert(TXT) cpp_assert (pfile, TXT)
+
+/* Define preprocessor macros for the -march and -mtune options.
+   PREFIX is either _LOONGARCH_ARCH or _LOONGARCH_TUNE, INFO is
+   the selected processor.  If INFO's canonical name is "foo",
+   define PREFIX to be "foo", and define an additional macro
+   PREFIX_FOO.  */
+#define LARCH_CPP_SET_PROCESSOR(PREFIX, CPU_TYPE)			      \
+  do									      \
+    {									      \
+      char *macro, *p;							      \
+      int cpu_type = (CPU_TYPE);					      \
+									      \
+      if (cpu_type == CPU_NATIVE)					      \
+	cpu_type = loongarch_native_cpu_type;				      \
+									      \
+      macro = concat ((PREFIX), "_", loongarch_cpu_strings[cpu_type], NULL);  \
+      for (p = macro; *p != 0; p++)					      \
+	*p = TOUPPER (*p);						      \
+									      \
+      builtin_define (macro);						      \
+      builtin_define_with_value ((PREFIX),				      \
+				 loongarch_cpu_strings[cpu_type], 1);	      \
+      free (macro);							      \
+    }									      \
+  while (0)
+
+/* TODO: what is the pfile technique ??? !!! */
+
+void
+loongarch_cpu_cpp_builtins (cpp_reader *pfile)
+{
+  builtin_assert ("machine=loongarch");
+  builtin_assert ("cpu=loongarch");
+  builtin_define ("__loongarch__");
+
+  LARCH_CPP_SET_PROCESSOR ("_LOONGARCH_ARCH", loongarch_cpu_arch);
+  LARCH_CPP_SET_PROCESSOR ("_LOONGARCH_TUNE", loongarch_cpu_tune);
+
+  /* Integer ISA/ABI */
+  if (TARGET_64BIT)
+    builtin_define ("__loongarch_gpr=64");
+
+  switch (loongarch_abi_int)
+    {
+    case ABI_LP64:
+      builtin_define ("_ABILP64=3");
+      builtin_define ("_LOONGARCH_SIM=_ABILP64");
+      builtin_define ("__loongarch64");
+      builtin_define ("__loongarch_lp64");
+      break;
+    }
+
+  /* Floating-Point ISA/ABI */
+  if (TARGET_DOUBLE_FLOAT)
+    builtin_define ("__loongarch_fpr=64");
+  else if (TARGET_SINGLE_FLOAT)
+    builtin_define ("__loongarch_fpr=32");
+  else
+    builtin_define ("__loongarch_fpr=0");
+
+  /* These defines reflect the ABI in use, not whether the
+     FPU is directly accessible.  */
+  if (TARGET_DOUBLE_FLOAT_ABI)
+    builtin_define ("__loongarch_double_float");
+  else if (TARGET_SINGLE_FLOAT_ABI)
+    builtin_define ("__loongarch_single_float");
+
+  if (TARGET_DOUBLE_FLOAT_ABI || TARGET_SINGLE_FLOAT_ABI)
+    builtin_define ("__loongarch_hard_float");
+  else
+    builtin_define ("__loongarch_soft_float");
+
+  /* Native Data Sizes */
+  builtin_define_with_int_value ("_LOONGARCH_SZINT", INT_TYPE_SIZE);
+  builtin_define_with_int_value ("_LOONGARCH_SZLONG", LONG_TYPE_SIZE);
+  builtin_define_with_int_value ("_LOONGARCH_SZPTR", POINTER_SIZE);
+  builtin_define_with_int_value ("_LOONGARCH_FPSET", 32 / MAX_FPRS_PER_FMT);
+  builtin_define_with_int_value ("_LOONGARCH_SPFPSET", 32);
+
+  /* Macros dependent on the C dialect.  */
+  if (preprocessing_asm_p ())
+    {
+      builtin_define_std ("LANGUAGE_ASSEMBLY");
+      builtin_define ("_LANGUAGE_ASSEMBLY");
+    }
+  else if (c_dialect_cxx ())
+    {
+      builtin_define ("_LANGUAGE_C_PLUS_PLUS");
+      builtin_define ("__LANGUAGE_C_PLUS_PLUS");
+      builtin_define ("__LANGUAGE_C_PLUS_PLUS__");
+    }
+  else
+    {
+      builtin_define_std ("LANGUAGE_C");
+      builtin_define ("_LANGUAGE_C");
+    }
+  if (c_dialect_objc ())
+    {
+      builtin_define ("_LANGUAGE_OBJECTIVE_C");
+      builtin_define ("__LANGUAGE_OBJECTIVE_C");
+      /* Bizarre, but retained for backwards compatibility.  */
+      builtin_define_std ("LANGUAGE_C");
+      builtin_define ("_LANGUAGE_C");
+    }
+}
