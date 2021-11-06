d4d432a3de4444afc3493472580dfb71f1151bb8
chenglulu <chenglulu@loongson.cn>
2021-11-06 12:51:06 +0800

    LoongArch Port: gcc
    gcc/
    
    	* common/config/loongarch/loongarch-common.c: New file.
    	* config/loongarch/constraints.md: New file.
    	* config/loongarch/generic.md: New file.
    	* config/loongarch/gnu-user.h: New file.
    	* config/loongarch/la464.md: New file.
    	* config/loongarch/larchintrin.h: New file.
    	* config/loongarch/linux.h: New file.
    	* config/loongarch/loongarch-builtins.c: New file.
    	* config/loongarch/loongarch-c.c: New file.
    	* config/loongarch/loongarch-cpu.c: New file.
    	* config/loongarch/loongarch-cpu.h: New file.
    	* config/loongarch/loongarch-driver.c: New file.
    	* config/loongarch/loongarch-driver.h: New file.
    	* config/loongarch/loongarch-ftypes.def: New file.
    	* config/loongarch/loongarch-modes.def: New file.
    	* config/loongarch/loongarch-opts.c: New file.
    	* config/loongarch/loongarch-opts.h: New file.
    	* config/loongarch/loongarch-protos.h: New file.
    	* config/loongarch/loongarch-rtx-cost.h: New file.
    	* config/loongarch/loongarch.c: New file.
    	* config/loongarch/loongarch.h: New file.
    	* config/loongarch/loongarch.md: New file.
    	* config/loongarch/loongarch.opt: New file.
    	* config/loongarch/predicates.md: New file.
    	* config/loongarch/sync.md: New file.
    	* config/loongarch/t-linux: New file.
    	* config/loongarch/t-loongarch: New file.
    	* config.gcc: Add LoongArch support.
    	* config/host-linux.c: Add LoongArch support.
    	* configure.ac: Add LoongArch support.
    	* doc/invoke.texi: Add LoongArch options section.
    	* doc/md.texi: Add LoongArch constraints section.

---
+++ gcc/config/loongarch/linux.h
@@ -0,0 +1,57 @@
+/* Definitions for LoongArch running Linux-based GNU systems with ELF format.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/* Default system library search paths.
+ * This ensures that a compiler configured with --disable-multilib
+ * can work in an multilib environment.  */
+
+#if defined (__DISABLE_MULTILIB) && defined (__DISABLE_MULTIARCH)
+
+  /* Integer ABI  */
+  #if DEFAULT_ABI_INT == ABI_LP64
+    #define INT_ABI_SUFFIX "lib64"
+  #endif
+
+  /* Floating-Point ABI  */
+  #if DEFAULT_ABI_FLOAT == ABI_SOFT_FLOAT
+    #define FLOAT_ABI_SUFFIX "soft/"
+  #elif DEFAULT_ABI_FLOAT == ABI_SINGLE_FLOAT
+    #define FLOAT_ABI_SUFFIX "single/"
+  #endif
+
+#endif
+
+#ifndef INT_ABI_SUFFIX
+#define INT_ABI_SUFFIX "lib"
+#endif
+
+#ifndef FLOAT_ABI_SUFFIX
+#define FLOAT_ABI_SUFFIX ""
+#endif
+
+#define STANDARD_STARTFILE_PREFIX_1 "/" INT_ABI_SUFFIX "/" FLOAT_ABI_SUFFIX
+#define STANDARD_STARTFILE_PREFIX_2 "/usr/" INT_ABI_SUFFIX "/" FLOAT_ABI_SUFFIX
+
+
+/* Define this to be nonzero if static stack checking is supported.  */
+#define STACK_CHECK_STATIC_BUILTIN 1
+
+/* FIXME*/
+/* The default value isn't sufficient in 64-bit mode.  */
+#define STACK_CHECK_PROTECT (TARGET_64BIT ? 16 * 1024 : 12 * 1024)
---
+++ gcc/config/loongarch/loongarch-cpu.h
@@ -0,0 +1,55 @@
+/* Definitions for LoongArch CPU properties.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   Contributed by Loongson Ltd.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef LOONGARCH_CPU_H
+#define LOONGARCH_CPU_H
+
+#include "loongarch-rtx-cost.h"
+
+/* enum loongarch_cpu_type */
+enum loongarch_cpu_type {
+    CPU_NATIVE = 0,
+    CPU_LOONGARCH64 = 1,
+    CPU_LA464 = 2,
+    N_CPU_TYPES = 3,
+};
+
+struct loongarch_isa_ext {
+    int ext_dummy;
+};
+
+struct loongarch_cpu_config {
+    int isa_int;
+    int isa_float;
+    struct loongarch_isa_ext ext;
+};
+
+/* Static CPU property tables */
+extern const char* loongarch_cpu_strings[];
+
+extern struct loongarch_cpu_config loongarch_cpu_default_config[];
+
+extern int loongarch_cpu_issue_rate[];
+
+extern int loongarch_cpu_multipass_dfa_lookahead[];
+
+extern struct loongarch_rtx_cost_data loongarch_cpu_rtx_cost_data[];
+
+#endif /* LOONGARCH_CPU_H */
