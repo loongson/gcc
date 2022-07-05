e116d4ddeea024ecf8897db3bfa338120015e26d
liuzhensong <liuzhensong@loongson.cn>
2022-07-05 20:13:38 +0800

    Fix NONE patch(huangpei).binutils commit 8b111532ea2f2cb407df74bbbcbd852ca242d3c2
    gcc/ChangeLog:
    
    	* config/loongarch/loongarch.h (ASM_PREFERRED_EH_DATA_FORMAT):

--- gcc/config/loongarch/loongarch.h
+++ gcc/config/loongarch/loongarch.h
@@ -1128,7 +1128,7 @@ struct GTY (()) machine_function
 #endif
 
 #define ASM_PREFERRED_EH_DATA_FORMAT(CODE, GLOBAL) \
-  (((GLOBAL) ? DW_EH_PE_indirect : 0) | DW_EH_PE_absptr)
+  (((GLOBAL) ? DW_EH_PE_indirect : 0) | DW_EH_PE_pcrel | DW_EH_PE_sdata4)
 
 /* Do emit .note.GNU-stack by default.  */
 #ifndef NEED_INDICATE_EXEC_STACK
